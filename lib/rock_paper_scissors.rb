87000559a5f57f0733b7b7aa037409d9e3759ae8
trinityXmontoya <trinityXmontoya@gmail.com>
2014-05-20 21:09:55 -0400

    generate_comp_choice test

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -1,6 +1,7 @@
 class RockPaperScissors
 
   attr_accessor :choices, :win_combos
+
   def initialize
     @choices = [:rock,:paper,:scissors]
     @win_combos = {
@@ -8,6 +9,57 @@ class RockPaperScissors
       :scissors => :paper,
       :paper => :rock
     }
+  end
+
+  def begin_game
+    puts
+    puts " ---------------------------------- "
+    puts "* WELCOME TO ROCK, PAPER, SCISSORS *"
+    puts " ---------------------------------- "
+    puts "Please choose one:"
+    puts "
+    Rock
+            _______
+        ---'   ____)
+              (_____)
+              (_____)
+              (____)
+        ---.__(___)
+
+
+    Paper
+            _______
+        ---'   ____)____
+                  ______)
+                  _______)
+                _______)
+        ---.__________)
+
+    Scissors
+            _______
+        ---'   ____)____
+                  ______)
+               __________)
+              (____)
+        ---.__(___)
+
+      "
+    user_input = gets.chomp.downcase.to_sym
+
+      while !choices.include? user_input
+        puts "Please choose a valid selection : rock, paper, or scissors"
+        user_input = gets.chomp.downcase.to_sym
+      end
+
+    # check_winner(user_input)
+
+  end
+
+  def generate_comp_choice
+    choices.sample
+  end
+
+  def check_winner(user_input)
   end
 
 end
--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -6,14 +6,32 @@ describe 'Rock Paper Scissors' do
   let(:new_game) {RockPaperScissors.new}
 
   context 'initialization' do
-    it 'should be initialized with the correct choices defined ' do
+    it 'should be initialized with the correct array of choices defined ' do
       expect(new_game.choices).to eq([:rock,:paper,:scissors])
     end
 
-    it 'should be initialized with the correct winning combos defined' do
+    it 'should be initialized with the correct winning combos hash defined' do
       expect(new_game.win_combos).to eq({:rock=>:scissors, :scissors=>:paper, :paper=>:rock})
     end
 
+  end
+
+  # describe '.begin_game' do
+  #
+  #   context 'validate that the user input is one of the given choices' do
+  #     it 'should prompt the user for a new input if the original one is invalid' do
+  #     STDIN.stub(:gets) {"r"}
+  #     expect(new_game.begin_game).to eq("Please choose a valid selection : rock, paper, or scissors")
+  #     end
+  #   end
+  #
+  #
+  # end
+
+  describe '.generate_comp_choice' do
+    it 'should return a random symbol from the choices array' do
+      expect(new_game.choices).to include(new_game.generate_comp_choice)
+    end
   end
 
 end
